651e8c00a21958797e03a6485681b8eb862843bd
AlessandroBufalino <alessandro2.bufalino@live.uwe.ac.uk>
2023-04-25 04:40:05 +0100

    New Website

---
+++ Javascript Scripts/Algorithms/sketch.js
@@ -0,0 +1,179 @@
+//region General Variables
+let currentState = '';
+let frameSkip = 5;
+let frameCounter = 0;
+//endregion
+
+const State = {
+  CA: 'CA',
+  PerlinNoise: 'PerlinNoise',
+  Default: 'Default'
+};
+
+let sizeSlider;
+
+function setup() {
+  let canvasWidth = windowWidth / 2;
+  let canvasHeight = 800;
+  let canvas = createCanvas(canvasWidth, canvasHeight);
+  canvas.parent('p5js-canvas');
+
+  updateControlElements();
+}
+
+function draw() {
+  background(220);
+
+  switch (currentState) {
+    case State.CA:
+    {
+
+      break;
+    }
+
+    case State.PerlinNoise:
+    {
+
+      break;
+    }
+
+    case State.Default:
+    {
+      let ellipseSize = sizeSlider.value(); // Get the value of the slider
+      ellipse(mouseX, mouseY, ellipseSize, ellipseSize);
+  
+      // Draw text in the middle of the canvas
+      textAlign(CENTER, CENTER);
+      textSize(24);
+      fill(0); // Set the text color to black
+      text('Sample Text', width / 2, height / 2);
+      break;
+    }
+
+    default:
+      {
+        
+        console.log('No control elements for current state');
+      }
+  }
+
+  frameCounter++;
+}
+
+
+function windowResized() {
+  let canvasWidth = windowWidth / 2;
+  resizeCanvas(canvasWidth, 800);
+}
+
+function buttonPressed() {
+  console.log('Button pressed!');
+}
+
+
+function updateControlElements() {
+  document.getElementById('control-container').innerHTML = '';
+
+  switch (currentState) {
+    case State.CA:
+      {
+        let slider1 = createLabeledSlider('Slider 1: ');
+        let slider2 = createLabeledSlider('Slider 2: ');
+        break;
+      }
+
+    case State.PerlinNoise:
+    {
+      for (let i = 1; i <= 6; i++) {
+        createLabeledSlider(`Slider ${i}: `);
+      }
+      break;
+    }
+
+    case State.Default:
+    {
+      sizeSlider = createLabeledSlider('Ellipse Size: ');
+      let exampleCheckbox = createLabeledCheckbox('Example Checkbox: ');
+      let exampleInputField = createLabeledInputField('Example Input: ');
+      let exampleButton = createLabeledButton('Example Button: ', buttonPressed);
+      let exampleBdutton = createLabeledButton('Example Button: ', buttonPressed);
+    }
+
+    default:
+      {
+        console.log('No control elements for current state');
+      }
+  }
+}
+
+
+
+function createLabeledSlider(labelText) {
+  let container = createDiv();
+  container.parent('control-container');
+
+  let label = createSpan(labelText);
+  label.parent(container);
+
+  let slider = createSlider(10, 200, 80);
+  slider.parent(container);
+
+  return slider;
+}
+
+function createLabeledCheckbox(labelText) {
+  let container = createDiv();
+  container.parent('control-container');
+
+  let label = createSpan(labelText);
+  label.parent(container);
+
+  let checkbox = createCheckbox();
+  checkbox.parent(container);
+
+  return checkbox;
+}
+
+function createLabeledInputField(labelText) {
+  let container = createDiv();
+  container.parent('control-container');
+
+  let label = createSpan(labelText);
+  label.parent(container);
+
+  let inputField = createInput();
+  inputField.parent(container);
+
+  return inputField;
+}
+
+function createLabeledButton(labelText, callback) {
+  let container = createDiv();
+  container.parent('control-container');
+
+  let label = createSpan(labelText);
+  label.parent(container);
+
+  let button = createButton('Click me');
+  button.mousePressed(callback);
+  button.parent(container);
+
+  return button;
+}
+
+
+
+function switchToCA() {
+  currentState = State.CA;
+  updateControlElements();
+}
+
+function switchToPerlinNoise() {
+  currentState = State.PerlinNoise;
+  updateControlElements();
+}
+
+function switchToDefault() {
+  currentState = State.Default;
+  updateControlElements();
+}
---
+++ Javascript Scripts/Main.js
@@ -0,0 +1,108 @@
+document.querySelectorAll('.scroll-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        const headerOffset = 0;
+        const elementPosition = target.offsetTop;
+        const offsetPosition = elementPosition - headerOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
+    });
+});
+
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    const halfWindowHeight = (window.innerHeight || document.documentElement.clientHeight) / 2;
+
+    // Check if the element is at the top of the viewport (first section)
+    if (rect.top >= 0 && rect.top <= halfWindowHeight) {
+        return true;
+    }
+
+    // Check if the element is at the bottom of the viewport (last section)
+    if (rect.bottom >= halfWindowHeight && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)) {
+        return true;
+    }
+
+    // Check if the element is in the middle of the viewport (other sections)
+    return rect.top <= halfWindowHeight && rect.bottom >= halfWindowHeight;
+}
+
+function changeBodyBackgroundColor() {
+    const sections = document.querySelectorAll('section');
+    const html = document.documentElement;
+
+    for (const section of sections) {
+        if (isElementInViewport(section)) {
+            const bgColor = getComputedStyle(section).backgroundColor;
+            html.style.backgroundColor = bgColor;
+            break;
+        }
+    }
+}
+
+
+function toggleGif(id, src) {
+    const imageElement = document.getElementById(id);
+    if (imageElement.src.includes(src)) {
+        const originalSrc = src.includes("gifs") ? src.replace("gifs", "pictures").replace(".gif", "_static.png") : src.replace("pictures", "gifs").replace("_static.png", ".gif");
+        imageElement.src = originalSrc;
+    } else {
+        imageElement.src = src;
+    }
+}
+
+function scrollToGridItem(itemId) {
+    const gridItem = document.getElementById(itemId);
+
+    if (gridItem) {
+        gridItem.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+        // Add a class to highlight the grid item
+        gridItem.classList.add('highlight');
+
+        // Remove the class after a certain duration (e.g. 3 seconds)
+        setTimeout(() => {
+            gridItem.classList.remove('highlight');
+        }, 3000);
+    }
+}
+
+function copyEmailToClipboard() {
+    const emailToCopy = document.getElementById('emailToCopy');
+    const copiedEmail = document.getElementById('copiedEmail');
+    const customPopup = document.getElementById('customPopup');
+    emailToCopy.select();
+    document.execCommand('copy');
+    copiedEmail.textContent = emailToCopy.value;
+    customPopup.classList.add('show');
+
+    setTimeout(() => {
+        customPopup.classList.remove('show');
+    }, 3000);
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.button-algo');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            const title = button.getAttribute('data-title');
+            const description = button.getAttribute('data-description');
+
+            document.getElementById('main-title').textContent = title;
+            document.getElementById('main-description').textContent = description;
+        });
+    });
+});
+
+// Call the function initially to set the background color
+document.addEventListener('DOMContentLoaded', changeBodyBackgroundColor);
+
+// Add the event listeners for scroll and resize events
+window.addEventListener('scroll', changeBodyBackgroundColor);
+window.addEventListener('resize', changeBodyBackgroundColor);
