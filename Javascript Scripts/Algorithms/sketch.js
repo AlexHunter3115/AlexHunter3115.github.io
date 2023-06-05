e7a319ff31e0d6293c5e726c9951f5d4ca285dc2
Alex <alessandro.bufalino3115@gmail.com>
2023-06-05 14:57:47 +0100

    added python and changed links

--- Javascript Scripts/Algorithms/sketch.js
+++ Javascript Scripts/Algorithms/sketch.js
@@ -1,129 +1,153 @@
-//region General Variables
-let currentState = '';
-let frameSkip = 5;
-let frameCounter = 0;
-//endregion
+class BaseAlgorithmStateClass {
+  UpdateElements() {}
 
-const State = {
-  CA: 'CA',
-  PerlinNoise: 'PerlinNoise',
-  Default: 'Default'
-};
+  UpdateCall() {}
 
-let sizeSlider;
+  DrawCall() {
+    //background(100); // Grey background
+    textAlign(CENTER, CENTER);
+    textSize(48);
+    fill(255, 0, 0); // Red text
+    text("BaseClass", width / 2, height / 2);
+  }
+
+  GuiDrawCall() {}
+
+  Select() {}
+
+  Deselect() {}
+}
+
+class CellularAutomata extends BaseAlgorithmStateClass {
+  
+  slider;
+  sizeOfelips = 15;
+  
+  UpdateElements() 
+  {
+    this.slider = createLabeledSlider("Size of Elipse")
+  }
+
+  UpdateCall() 
+  {
+    fill(128, 128, 0)
+    ellipse(mouseX, mouseY, 50, this.slider.value());
+  }
+
+  DrawCall() {
+    //background(250); // Grey background
+    textAlign(CENTER, CENTER);
+    textSize(48);
+    fill(255, 0, 0); // Red text
+    text("Cell", width / 2, height / 2);
+  }
+
+  GuiDrawCall() {}
+
+  Select() {}
+
+  Deselect() {}
+}
+
+class Test extends BaseAlgorithmStateClass {
+  UpdateElements() {}
+
+  UpdateCall() {}
+
+  DrawCall() {
+    //background(200); // Grey background
+    textAlign(CENTER, CENTER);
+    textSize(48);
+    fill(255, 0, 0); // Red text
+    text("test", width / 2, height / 2);
+  }
+
+  GuiDrawCall() {}
+
+  Select() {}
+
+  Deselect() {}
+}
+
+class PerlinNoise extends BaseAlgorithmStateClass {
+  UpdateElements() {}
+
+  UpdateCall() {}
+
+  DrawCall() {
+    
+    textAlign(CENTER, CENTER);
+    textSize(48);
+    fill(255, 0, 0); // Red text
+    text("perlin", width / 2, height / 2);
+  }
+
+  GuiDrawCall() {}
+
+  Select() {}
+
+  Deselect() {}
+}
+
+let allStatesArr = [
+  new BaseAlgorithmStateClass(),
+  new CellularAutomata(),
+  new PerlinNoise(),
+  new Test(),
+];
+let currentAlgoIndex = 0;
 
 function setup() {
   let canvasWidth = windowWidth / 2;
   let canvasHeight = 800;
   let canvas = createCanvas(canvasWidth, canvasHeight);
-  canvas.parent('p5js-canvas');
+  canvas.parent("p5js-canvas");
 
   updateControlElements();
 }
-
-function draw() {
-  background(220);
-
-
-  if ( window.isFrozen )
-  {
-    background(200); // Grey background
-        textAlign(CENTER, CENTER);
-        textSize(48);
-        fill(255, 0, 0); // Red text
-        text('Frozen', width / 2, height / 2);
-        //console.log('Frozen');
-  }
-  else
-  {
-
-  switch (currentState) {
-    case State.CA:
-    {
-
-      break;
-    }
-
-    case State.PerlinNoise:
-    {
-
-      break;
-    }
-
-    case State.Default:
-    {
-      let ellipseSize = sizeSlider.value(); // Get the value of the slider
-      ellipse(mouseX, mouseY, ellipseSize, ellipseSize);
-  
-      // Draw text in the middle of the canvas
-      textAlign(CENTER, CENTER);
-      textSize(24);
-      fill(0); // Set the text color to black
-      text('Sample Text', width / 2, height / 2);
-      break;
-    }
-
-    default:
-      {
-        
-        //console.log('No control elements for current state');
-      }}
-  }
-
-  frameCounter++;
-}
-
 
 function windowResized() {
   let canvasWidth = windowWidth / 2;
   resizeCanvas(canvasWidth, 800);
 }
 
-function buttonPressed() {
-  console.log('Button pressed!');
-}
-
-
-function updateControlElements() {
-  document.getElementById('control-container').innerHTML = '';
-
-  switch (currentState) {
-    case State.CA:
-      {
-        let slider1 = createLabeledSlider('Slider 1: ');
-        let slider2 = createLabeledSlider('Slider 2: ');
-        break;
-      }
-
-    case State.PerlinNoise:
-    {
-      for (let i = 1; i <= 6; i++) {
-        createLabeledSlider(`Slider ${i}: `);
-      }
-      break;
-    }
-
-    case State.Default:
-    {
-      sizeSlider = createLabeledSlider('Ellipse Size: ');
-      let exampleCheckbox = createLabeledCheckbox('Example Checkbox: ');
-      let exampleInputField = createLabeledInputField('Example Input: ');
-      let exampleButton = createLabeledButton('Example Button: ', buttonPressed);
-      let exampleBdutton = createLabeledButton('Example Button: ', buttonPressed);
-    }
-
-    default:
-      {
-        //console.log('No control elements for current state');
-      }
+function UpdateCall() {
+  if (!window.isFrozen) {
+    allStatesArr[currentAlgoIndex].UpdateCall();
   }
 }
 
+function DrawCall() {
+  if (window.isFrozen) {
+    background(200); // Grey background
+    textAlign(CENTER, CENTER);
+    textSize(48);
+    fill(255, 0, 0); // Red text
+    text("Frozen", width / 2, height / 2);
+    //console.log('Frozen');
+  } else {
+    allStatesArr[currentAlgoIndex].DrawCall();
+  }
+}
+
+function GuiDrawCall() {
+  if (!window.isFrozen) {
+    allStatesArr[currentAlgoIndex].GuiDrawCall();
+  }
+}
+
+function draw() {
+  background(220);
+
+  UpdateCall();
+  DrawCall();
+  GuiDrawCall();
+}
 
 
 function createLabeledSlider(labelText) {
   let container = createDiv();
-  container.parent('control-container');
+  container.parent("control-container");
 
   let label = createSpan(labelText);
   label.parent(container);
@@ -136,7 +160,7 @@ function createLabeledSlider(labelText) {
 
 function createLabeledCheckbox(labelText) {
   let container = createDiv();
-  container.parent('control-container');
+  container.parent("control-container");
 
   let label = createSpan(labelText);
   label.parent(container);
@@ -149,7 +173,7 @@ function createLabeledCheckbox(labelText) {
 
 function createLabeledInputField(labelText) {
   let container = createDiv();
-  container.parent('control-container');
+  container.parent("control-container");
 
   let label = createSpan(labelText);
   label.parent(container);
@@ -162,31 +186,25 @@ function createLabeledInputField(labelText) {
 
 function createLabeledButton(labelText, callback) {
   let container = createDiv();
-  container.parent('control-container');
+  container.parent("control-container");
 
   let label = createSpan(labelText);
   label.parent(container);
 
-  let button = createButton('Click me');
+  let button = createButton("Click me");
   button.mousePressed(callback);
   button.parent(container);
 
   return button;
 }
 
+function switchAlgoState(index) {
+  allStatesArr[currentAlgoIndex].Deselect();
+  
+  currentAlgoIndex = index;
 
+  document.getElementById("control-container").innerHTML = "";
+  allStatesArr[currentAlgoIndex].UpdateElements();
 
-function switchToCA() {
-  currentState = State.CA;
-  updateControlElements();
-}
-
-function switchToPerlinNoise() {
-  currentState = State.PerlinNoise;
-  updateControlElements();
-}
-
-function switchToDefault() {
-  currentState = State.Default;
-  updateControlElements();
+  allStatesArr[currentAlgoIndex].Select();
 }
