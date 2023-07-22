ac34cb567841dcc7ccdd40bdc55386ad824c5542
Alex <alessandro.bufalino3115@gmail.com>
2023-07-22 11:52:18 +0100

    added new cv amend

--- server_scripts/server.js
+++ server_scripts/server.js
@@ -44,4 +44,4 @@ function writeScores(req, res) {
 app.get('/scores', getScores);
 app.post('/scores', writeScores);
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3000, () => console.log('Server listening on port 3000'));
